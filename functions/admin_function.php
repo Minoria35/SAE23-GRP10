6686429a15ac28f33c25b228764d6d3fe60fa2f1
Victor Cocard <133344733+Viictor56@users.noreply.github.com>
2023-06-08 19:24:32 +0200

    Update admin_function.php
    ajout des fonctions addPartner et deletePartner

--- functions/admin_function.php
+++ functions/admin_function.php
@@ -89,5 +89,25 @@
         </script>
         ';
     }
+function addPartner($partenaire) {
+
+    $file = 'data/partenaires.json';
+    $json = file_get_contents($file);
+    $users = json_decode($json, true);
+    $users[$partenaire] = [
+        "partenaire" => $partenaire
+    ];
+    $json = json_encode($users);
+    file_put_contents($file, $json);
+}
+
+function deletePartner($partenaire_suppr) {
+    $file = 'data/partenaires.json';
+    $json = file_get_contents($file);
+    $users = json_decode($json, true);
+    unset($users[$partenaire_suppr]);
+    $json = json_encode($users);
+    file_put_contents($file, $json);
+}
 
 ?>
