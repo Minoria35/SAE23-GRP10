cd26af07bf9165ba35ec90a99b528879994564de
Victor Cocard <133344733+Viictor56@users.noreply.github.com>
2023-06-08 20:37:00 +0200

    Update suppression_partenaire.php
    enlèvement du echo et rajout d'un exit pour exit quand le nom du partenaire n'a pas été rempli

--- functions/traitement_function/suppression_partenaire.php
+++ functions/traitement_function/suppression_partenaire.php
@@ -1,12 +1,13 @@
 <?php
 include '..\admin_function.php';
 $suppression_partenaire = $_POST['partenaire_suppr'];
-echo $suppression_partenaire;
 if (empty($suppression_partenaire)) {
     echo "Le nom du partenaire n'a pas été rempli. Vous allez être redirigé vers la page de création de partenaires.";
-    header('Refresh: 5; URL=..\Intranet\gestion_partner.php');}
+    header('Refresh: 5; URL=..\..\Intranet\gestion_partner.php');
+    exit();}
 
 deletePartner($suppression_partenaire);
-header('Location: ..\Intranet\gestion_partner.php');
+header('Location: ..\..\Intranet\gestion_partner.php');
 exit();
+
 ?>
