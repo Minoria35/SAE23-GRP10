64130d65b5959cdb70a725da67ea022bac7bc16b
Victor Cocard <133344733+Viictor56@users.noreply.github.com>
2023-06-08 19:47:05 +0200

    Update creation_partenaire.php
    update de l'URL de redirection vers ..\Intranet\gestion_partner.php

--- functions/traitement_function/creation_partenaire.php
+++ functions/traitement_function/creation_partenaire.php
@@ -4,7 +4,7 @@ $creation_partenaire = $_POST['partenaire'];
 
 if (empty($creation_partenaire)) {
     echo "Le nom du partenaire n'a pas été rempli. Vous allez être redirigé vers la page de création de partenaires.";
-    header('Refresh: 5; URL=page07.php');
+    header('Refresh: 5; URL=..\Intranet\gestion_partner.php');
     exit();
 }
 addPartner($creation_partenaire);
