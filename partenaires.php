137db0c835c21976f53bc43bdef693b9bcb0a257
Victor Cocard <133344733+Viictor56@users.noreply.github.com>
2023-06-08 19:30:51 +0200

    Update partenaires.php
    refonte totale avec affichage des partenaires depuis le fichier partenaires.json

--- partenaires.php
+++ partenaires.php
@@ -1,57 +1,37 @@
 <?php
+include 'functions\admin_function.php';
+setup('Partenaires');
+pageheader('Partenaires', './partenaires.php');
+navbar('Partenaires');
 
-    include 'functions\function.php';
-    setup('Partenaires');
-    pageheader('Partenaires', './partenaires.php');
-    navbar('Partenaires');
+if (!authentifie()) {
+    header('Location: connexion_user.php');
+}
 ?>
 
-<div class="container-fluid row row-cols-1 row-cols-md-2 g-3" style="margin-top: 60px;">
-        <div class="col">
-            <div class="card h-100">
-                <img class="card-img-top" src="images/Powermedia.jpg" alt="Card image" style="width:80%">
-                <div class="card-body">
-                    <h4 class="card-title">Power Média</h4>
-                    <p class="card-text">Power Média est une petite entreprise Rennaise de reconditionnement
-                        d'ordinateur portable et de smartphone. Je vous parle d'eux puisque j'ai effectuer mon premier
-                        stage en seconde avec eux.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img class="card-img-top" src="images/INRAE.png" alt="Card image" style="width:80%">
-                <div class="card-body">
-                    <h4 class="card-title">INRAE</h4>
-                    <p class="card-text">INRAE est l'Institut National de Recherche pour l'Agriculture, l'Alimentation
-                        et l'Environnement. Je vous parle d'eux puisque j'ai effectuer mon
-                        premier stage de première avec M. MAUBERT (Administrateur Réseau).</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img class="card-img-top" src="images/Hexatel.png" alt="Card image" style="width:80%">
-                <div class="card-body">
-                    <h4 class="card-title">Hexatel</h4>
-                    <p class="card-text">Hexatel est un Opérateur télécom et réseaux. Je vous parle d'eux puisque j'ai
-                        effectuer mon deuxième
-                        stage de première aux sein de leur Centre d'Expertise Technique.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img class="card-img-top" src="images/RMetropole.png" alt="Card image" style="width:80%">
-                <div class="card-body">
-                    <h4 class="card-title">Rennes Métropoles</h4>
-                    <p class="card-text">Finalement, j'ai réaliser mon stage de terminale à Rennes Métropoles, au sein
-                        de l'unité télécom du service Infrastructure de la Direction des Systèmes d'Informations.</p>
-                </div>
-            </div>
-        </div>
-</div>
+<br>
+    <h1 class="my-4 text-center">Liste des partenaires</h1 >
 
-<?php
-    footer();
-?>
+    <div class="container">
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Partenaire</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php
+                    $partenaires = json_decode(file_get_contents("data/partenaires.json"), true);
+                    foreach ($partenaires as $user) {
+                    ?>
+                        <tr>
+                            <td><?php echo $user['partenaire'] ?></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
