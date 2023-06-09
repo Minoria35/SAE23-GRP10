cf0860575f930179a3f2642036e43103568c7c04
Victor Cocard <133344733+Viictor56@users.noreply.github.com>
2023-06-09 03:13:25 +0200

    Update index.php
    ajout d'une présentation de l'accueil

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
     navbar('Accueil');
 
 ?>
-
+<h2 class="px-4">Bienvenue chez Plaprêt, votre destination culinaire pour des plats préparés de qualité supérieure !</h2>
+<br>
+<p class="px-3"> Nous sommes une entreprise passionnée par l'art de la cuisine et dévouée à offrir à nos clients une expérience gastronomique inoubliable, le tout dans le confort de leur foyer.
+<br><br>
+Chez Plaprêt, nous croyons fermement que le plaisir de bien manger ne devrait pas être compromis par des emplois du temps chargés. C'est pourquoi nous mettons tout en œuvre pour créer des plats préparés savoureux, équilibrés et faciles à préparer. Notre équipe talentueuse de chefs cuisiniers hautement qualifiés sélectionne soigneusement les ingrédients les plus frais et les plus sains pour chaque recette, en veillant à ce que chaque bouchée soit une explosion de saveurs.
+<br><br>
+Nous comprenons que chaque personne a des goûts et des préférences uniques. C'est pourquoi Plaprêt propose une large gamme de plats préparés, allant des délices méditerranéens aux mets asiatiques, en passant par les classiques de la cuisine internationale. Que vous soyez végétarien, végétalien, adepte de la viande ou intolérant à certains ingrédients, nous avons des options qui répondront à vos besoins alimentaires spécifiques.
+Ensemble, nous créons une expérience culinaire qui vous permet de profiter des plaisirs de la bonne cuisine sans compromis. Rejoignez-nous chez Plaprêt et découvrez une nouvelle façon pratique et délicieuse de vous régaler avec nos plats préparés.</p>
 <?php
     footer();
 ?>
